abe356cbdd5f97a971ba7c2a005e34c8e2ce62c0
tahaminamostafa.chow <tahaminamostafa.chow@IC-CS233-4.uc.ucalgary.ca>
2022-08-13 03:34:29 -0600

    created two class one for UserWorkout for people who choose to produce a workout routine the other one is Day which tells ther user which day which workout and for hpow many hours.

---
+++ src/application/UserWorkout.java
@@ -0,0 +1,14 @@
+package application;
+
+public class UserWorkout extends User {
+
+	public UserWorkout(String memberFirst, String memberLast, String memberAge, String memberGender, String height,
+			String memberCurrentWeight, String memberPassword) {
+		super(memberFirst, memberLast, memberAge, memberGender, height, memberCurrentWeight, memberPassword);
+		
+	}
+	
+	
+	
+
+}
