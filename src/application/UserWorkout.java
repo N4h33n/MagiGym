999a2b67615ec05399821c231b840b1b23f75553
tahaminamostafa.chow <tahaminamostafa.chow@IC-CS233-38.uc.ucalgary.ca>
2022-08-14 02:13:58 -0600

    created userWorkout and also Day for users who choose to plan a user workout. made a constructor in userworkout and instamce variable,and encapsulated them.

--- src/application/UserWorkout.java
+++ src/application/UserWorkout.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.ArrayList;
+
 public class UserWorkout extends User {
 
 	public UserWorkout(String memberFirst, String memberLast, String memberAge, String memberGender, String height,
@@ -7,6 +9,21 @@ public class UserWorkout extends User {
 		super(memberFirst, memberLast, memberAge, memberGender, height, memberCurrentWeight, memberPassword);
 		
 	}
+	
+	
+	
+	private ArrayList<Day> day;
+	
+	ArrayList<Day> getDay() {
+		return day;
+		
+	
+	}
+	void setDay(ArrayList<Day> day) {
+		this.day = day;
+	}
+	
+	
 	
 	
 	
